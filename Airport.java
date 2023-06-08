bd083c5398a70c17e2f8ffd96744871774df588f
Hariharan <hariharansxworkz@gmail.com>
2023-06-08 10:41:37 +0530

    Adding single file

---
+++ Airport.java
@@ -0,0 +1,22 @@
+class Airport{
+
+public static void arrival(){
+	
+System.out.println("This is arrival");	
+	
+}
+public static void department(){
+arrival();	
+System.out.println("This is department");	
+	
+}
+public static void checking(){
+department();
+
+System.out.println("This is checking");	
+	
+}
+
+
+
+}
